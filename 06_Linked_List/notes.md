8fc349ef92d3b9f646236cdffad87f5f40363ef5
Malay Raval <Malay.raval11@gmail.com>
2026-07-17 10:15:00 +0000

    feat: add linked list problems - reverse list and Floyd cycle detection

---
+++ 06_Linked_List/notes.md
@@ -0,0 +1,3 @@
+# Linked List
+- Reverse: O(n) time, O(1) space - iterative with 3 pointers
+- Cycle Detection: Floyd's Tortoise and Hare - O(n) time, O(1) space
---
+++ 06_Linked_List/solutions.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* curr = head;
+    while (curr) {
+        ListNode* nextTemp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+}
+
+bool hasCycle(ListNode* head) {
+    if (!head || !head->next) return false;
+    ListNode* slow = head;
+    ListNode* fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) return true;
+    }
+    return false;
+}
